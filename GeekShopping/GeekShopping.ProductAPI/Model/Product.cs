0e5f9ea48b229e6d95ffd509804a56e027163164
rafaela montalvao <rafaelainocencioo@gmail.com>
2023-09-07 17:32:33 -0300

    git feat: criando model de Produto e sua Migration

---
+++ GeekShopping/GeekShopping.ProductAPI/Model/Product.cs
@@ -0,0 +1,27 @@
+﻿using GeekShopping.ProductAPI.Model.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.ProductAPI.Model
+{
+    public class Product : BaseEntity
+    {
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0, 10000)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength (50)]
+        public string CategoryName { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
