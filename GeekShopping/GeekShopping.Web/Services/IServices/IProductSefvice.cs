d4b72e8dcfdd0002278eec56c52a49afcf43282a
rafaela montalvao <rafaelainocencioo@gmail.com>
2023-09-08 22:59:37 -0300

    git feat: implementendo HttpClientExtentions

---
+++ GeekShopping/GeekShopping.Web/Services/IServices/IProductSefvice.cs
@@ -0,0 +1,14 @@
+﻿using GeekShopping.Web.Models;
+using Microsoft.AspNetCore.DataProtection.KeyManagement.Internal;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface IProductSefvice
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+        Task<ProductModel> FindById(long id);
+        Task<ProductModel> CreateProduct(ProductModel model);
+        Task<ProductModel> UpdateProduct(ProductModel model);
+        Task<bool> DeleteProductById(long id);
+    }
+}
